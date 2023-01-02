f7f3c9fc0612e7557c18236f345679bb29f46a73
ChrisV2 <chris.kagabe@gmail.com>
2023-01-02 21:50:13 +0200

    Uniswap interfaces

--- packages/hardhat/contracts/core/interfaces/IFactory.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.17;
-
-interface IFactory {
-    function getPair(address tokenA, address tokenB) external view returns(address pair);
-    function createPair(address tokenA, address tokenB) external returns(address pair);
-}
-    
